2239c4998ff67848eb398138fcf967fff25ab245
nebiye.arslan <nebiyearslan1@gmail.com>
2025-06-05 09:56:52 +0100

    tdd

--- build.sbt
+++ build.sbt
@@ -5,4 +5,17 @@ ThisBuild / scalaVersion := "2.13.16"
 lazy val root = (project in file("."))
   .settings(
     name := "QA Academy Lessons"
+
   )
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "3.2.19" % Test,
+  "org.scalactic" %% "scalactic" % "3.2.19",
+
+)
+
+
+
+
+
+
+
---
+++ src/main/scala/Calculator.scala
@@ -0,0 +1,21 @@
+class Calculator {
+
+
+  def add(x: Int, y: Int): Int = x + y
+
+  def multiply(x: Int, y: Int): Int = x * y
+
+  def subtract(x: Int, y: Int): Int = x - y
+
+
+  //def BenefitCalculator(income: Int, children: List[Child]): Double = 26.05
+  // def calculateWeeklyBenefit(income: Int, children: List[Child]): Double = 26.05
+
+  def calculateWeeklyBenefit(income: Int, children: List[Child]): Double = 26.05
+
+
+  }
+
+
+
+
