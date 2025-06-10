fa2f059e4a1a6eb31773fd65f81ab0ed2bb3e771
nebiye.arslan <nebiyearslan1@gmail.com>
2025-06-10 18:10:03 +0100

    DAy6 task

---
+++ src/main/scala/Automation/Wekk6Day2/Task1.scala
@@ -0,0 +1,21 @@
+package Automation.Wekk6Day2
+
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+
+object Task1 extends App {
+
+  val driver: WebDriver = new ChromeDriver() //1. Launch the browser using WebDriver
+  driver.get("https://demoqa.com/browser-windows") //2. Navigate to the test URL.
+  println("navigate page")
+
+
+  println("Page Title: " + driver.getTitle) // Page title
+  println("Current URL: " + driver.getCurrentUrl) // Current URL
+
+  driver.close()
+
+  // 5. End the session
+  driver.quit()
+  
+}
